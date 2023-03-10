885698625d0530d2b00e6b3f8af81f17a4447fa2
Alex <sasha-boriskov@yandex.ru>
2023-03-10 23:45:32 +0300

    Программа по поиску min и max из двух чисел

---
+++ Task002_max/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write ("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine ());
+Console.Write ("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine ());
+int max = a;
+int min = a;
+
+if (b>max)
+{
+    max=b;
+    Console.Write ("Максимальное число: ");
+    Console.WriteLine (max);
+    Console.Write ("Минимальное число: ");
+    Console.WriteLine (min);
+}
+else
+{
+    min=b;
+    Console.Write ("Максимальное число: ");
+    Console.WriteLine (max);
+    Console.Write ("Минимальное число: ");
+    Console.WriteLine (min);
+}
