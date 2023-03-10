7f391b397e999e75289c230fba4f7096c4bc455b
Alex <sasha-boriskov@yandex.ru>
2023-03-10 23:55:28 +0300

    Поиск max числа из трех рандомных чисел

---
+++ Task004_max3/Program.cs
@@ -0,0 +1,14 @@
+﻿double a = new Random().Next (-1000,1000);
+Console.WriteLine (a);
+double b = new Random().Next (-1000,1000);
+Console.WriteLine (b);
+double c= new Random ().Next(-1000,1000);
+Console.WriteLine (c);
+
+double max = a;
+
+if (b>max) {max=b;}
+if (c>max) {max=c;}
+
+Console.Write("Максимальное число = ");
+Console.WriteLine(max);
